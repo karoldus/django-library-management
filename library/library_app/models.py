7867efd191c5bf11ca65da93d998861e7735b11f
Karol Duszczyk <karol.duszczyk.stud@pw.edu.pl>
2021-09-01 10:23:53 +0200

    borrower - change model and books sides

--- library/library_app/models.py
+++ library/library_app/models.py
@@ -1,6 +1,7 @@
 # from typing_extensions import Required
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class BookType(models.Model):
@@ -13,7 +14,7 @@ class BookType(models.Model):
 class Book(models.Model):
     type = models.ForeignKey(BookType, on_delete=models.CASCADE)
     production_year = models.IntegerField()
-    avalible = models.BooleanField(default=True)
+    borrower = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def get_absolute_url(self):
         return f"/books/{self.id}"
